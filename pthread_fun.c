a39025e53cbfa1977d1695348dfc78d3e4364bd4
mejjete <dani1.mlp1@gmail.com>
2019-06-12 16:45:11 +0300

    thread problem (sync)

--- pthread_fun.c
+++ pthread_fun.c
@@ -9,36 +9,55 @@
 #include <string.h>
 #include <unistd.h>
 #include <linux/sched.h>
+//#include "pthread.h"
 
 volatile bool is_finished = false;
-volatile int counter = 0;
+int counter = 0;
 
 int thread_create_clone(int (*func)(void *), void *arg, void **stack);
+//int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start)(void *), void *arg);
+
 
 int thread_f(void *arg)
 {
-    /*pid_t thread_id = getpid();
+    pid_t thread_id = getpid();
     pid_t pid = getpid();
     printf("pid %d, tid %d: new thread, arg = %d\n", (int)pid, (int)thread_id, *((int*)arg));
-    is_finished = true;*/
+    is_finished = true;
+    return 0;
+}
+
+int thread_d(void *arg)
+{
+    printf("child thread\n");
+    int i;
+    for(i = 0; i < 100000; ++i)
+        __sync_add_and_fetch(&counter, 1);
+    is_finished = true;
     return 0;
 }
 
 int main()
 {   
-    pid_t thread_id = getpid();
-    pid_t pid = getpid();
-    printf("pid: %d, tid: %d new thread\n", (int)pid, (int)thread_id);
+    printf("test\n");
+    //pid_t thread_id = getpid();
+    //pid_t pid = getpid();
+    //printf("pid: %d, tid: %d new thread\n", (int)pid, (int)thread_id);
     void *stack;
     int arg = 100;
-    int ret = thread_create_clone(thread_f, (void*) &arg, &stack);
-    printf("pid %d, tid %d: clone result = %d\n", (int)pid, (int)thread_id, ret);
+    int ret = thread_create_clone(thread_d, NULL, &stack);
+    for(int i = 0; i < 100000; ++i)
+        __sync_add_and_fetch(&counter, 1);
     while(!is_finished)
         sched_yield();
+    printf("counter %d\n", counter);
+    //printf("pid %d, tid %d: clone result = %d\n", (int)pid, (int)thread_id, ret);
+    /*for(int i = 0; i < 100000; i++)
+        __sync_add_and_fetch(&counter, 1);
+    printf("counter: %d", counter);
     free(stack);
-    return 0;
+    return 0;*/
 }
-
 
 int thread_create_clone(int (*func)(void *), void *arg, void **stack)
 {
@@ -49,7 +68,11 @@ int thread_create_clone(int (*func)(void *), void *arg, void **stack)
     return clone(func, stack_top, flags, arg);
 }
 
-int a()
+/*int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start)(void *), void *arg)
 {
-    return 0;
-}
+    int stack_size = 65 * 1024;
+    void *stack = malloc(stack_size);
+    *stack_top = (char *) *stack + stack_size;
+    int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD; 
+    return clone(start, stack_top, flags, attr);
+}*/
