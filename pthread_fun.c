9d99438e9bfc98071200435b8bcfaa0e4f5fc335
mejjete <dani1.mlp1@gmail.com>
2019-06-12 17:48:49 +0300

    add spinlock

--- pthread_fun.c
+++ pthread_fun.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-//#include <syscall.h>
+#include <syscall.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/utsname.h>
@@ -9,14 +9,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <linux/sched.h>
+#include <linux/futex.h>
 //#include "pthread.h"
 
 volatile bool is_finished = false;
 int counter = 0;
 
 int thread_create_clone(int (*func)(void *), void *arg, void **stack);
-//int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start)(void *), void *arg);
+void spin_lock(volatile bool *lock);
+void spin_unlock(volatile bool *lock);
 
+//int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start)(void *), void *arg);
 
 int thread_f(void *arg)
 {
@@ -51,6 +54,7 @@ int main()
     while(!is_finished)
         sched_yield();
     printf("counter %d\n", counter);
+    free(stack);
     //printf("pid %d, tid %d: clone result = %d\n", (int)pid, (int)thread_id, ret);
     /*for(int i = 0; i < 100000; i++)
         __sync_add_and_fetch(&counter, 1);
@@ -59,13 +63,46 @@ int main()
     return 0;*/
 }
 
+int futex_wait(int *futex, int val)
+{
+    return syscall(SYS_futex, futex, FUTEX_WAIT, val, NULL, NULL, 0);
+}
+
+int futex_wake(int *futex)
+{
+    return syscall(SYS_futex, futex, FUTEX_WAKE, 1, NULL, NULL, 0);
+}
+
+void futex_lock(int *futex)
+{
+    while(__sync_val_compare_and_swap(futex, 0, 1) !== 0)
+        futex_wait(futex, 1);
+}
+
+void *futex_unlock(int *futex)
+{
+    __sync_bool_compare_and_swap(futex, 1, 0);
+    futex_wake(futex);
+}
+
 int thread_create_clone(int (*func)(void *), void *arg, void **stack)
 {
     int stack_size = 65 * 1024;
     *stack = malloc(stack_size);
     void *stack_top = (char *) *stack + stack_size;
     int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD;
-    return clone(func, stack_top, flags, arg);
+    return clone(func, stack_top, flags, arg); 
+}
+
+void spin_lock(volatile bool *lock)
+{
+    while(!__sync_bool_compare_and_swap(lock, 0, 1))
+        ;
+}
+
+void spin_unlock(volatile bool *lock)
+{
+    __sync_bool_compare_and_swap(lock, 1, 0);
 }
 
 /*int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start)(void *), void *arg)
@@ -74,5 +111,6 @@ int thread_create_clone(int (*func)(void *), void *arg, void **stack)
     void *stack = malloc(stack_size);
     *stack_top = (char *) *stack + stack_size;
     int flags = CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND | CLONE_THREAD; 
-    return clone(start, stack_top, flags, attr);
+    *thread = (pthread_t*)clone(start, stack_top, flags, attr);
+    return *thread == 0 ? 0 : 1;
 }*/
